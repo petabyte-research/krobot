9454d8d6f2f10554290af1a1f05d97c31218e145
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2016-01-14 19:28:50 +0100

    Additional new features and complete redesign
    - redesigned w/ Bootstrap
    - Lists uploaded files on front page
    - Shows count of remaining blocks
    
    See `modifications-juzraai.md` for details.

--- modifications-juzraai.md
+++ modifications-juzraai.md
@@ -5,7 +5,25 @@ According to K-Monitor's needs I made some modifications on this lil' kool tool:
 `service.py`:
 * Added regex testing of "tax_nr" and "org_id" before addition, to ensure the result XML will not contain empty or invalid values.
 * Added searching for "tender" blocks without "org_id"/"tax_nr" to extend the tool's functionality.
-* Modified it to preserve original filename.
+* Modified it to preserve original filename by storing it in the id.
+* Saves also the timestamp of upload to inform the user.
+* Also added a bunch of values to the rendered pages to provide more information for the user.
+
+`templates/*.html`:
+* Redesigned the whole thing using Bootstrap and Bootswatch Simplex theme.
+* I changed nearly the whole HTML structure.
+
+`templates/base.html`:
+* Version number :-)
+* Page title now shows filename.
+* Header is followed by a breadcrumb thing to show the 3 main steps and the current filename to the user.
+* Added a footer with links to the original and the forked repo.
+
+`templates/download.html`:
+* Added a sentence containing a link to the front page.
+
+`templates/index.html`:
+* Added a new panel below upload function where previously uploaded files are listed with upload time and action button (edit/download).
 
 `templates/process.html`:
 * Made some code formatting to increase readibilty.
@@ -13,9 +31,8 @@ According to K-Monitor's needs I made some modifications on this lil' kool tool:
 * Added fields "county", "region", "palyazo" to extend the tool's functionality.
 * Enclosed all field blocks in if-s to print only what is existing.
 * Moved "zip_code" to the first place.
-
-`templates/base.html`:
-* Version number :-)
+* Save button tells the user how many blocks are waiting for input.
+* I added a hint with a front page link.
 
 Also I added `start.sh` and `stop.sh` to make it easier to run this thing in background.
 
--- service.py
+++ service.py
@@ -4,93 +4,104 @@ import web
 import lxml.etree
 import jinja2
 import re
+import datetime
 
 e=jinja2.Environment(loader=jinja2.FileSystemLoader('templates'));
 
 urls= (
-    '/', 'index',
-    '/file/(.+)', 'process',
-    '/download/(.+)', 'download',
-    )
+	'/', 'index',
+	'/file/(.+)', 'process',
+	'/download/(.+)', 'download',
+	)
 
 data= {}
 
 class index:
-    """ handle requests to the page """
-    def GET(self):
-        t=e.get_template("index.html")
-        return t.render()
+	""" handle requests to the page """
+	def GET(self):
+		t=e.get_template("index.html")
+		files = []
+		for id in data:
+			files.append({
+				"id": id,
+				"filename": id.split('::')[0],
+				"uploaded": id.split('::')[1],
+				"elcount": len(process().get_elements(id))
+			})
+		return t.render({ "active": 0,"files": list(reversed(files)) })
 
-    def POST(self):
-        x = web.input(file={})
-        content = x['file'].file.read()
-        h = hashlib.md5(content).hexdigest()
-        h = x['file'].filename.split('/')[-1] + '::' + h
-        try:
-            data[h]=lxml.etree.fromstring(content)
-        except lxml.etree.XMLSyntaxError as ex:
-            t=e.get_template("index.html")
-            return t.render({"error": """Error Parsing XML: %s"""%ex})
-        raise web.seeother('/file/%s'%h)
+	def POST(self):
+		x = web.input(file={})
+		content = x['file'].file.read()
+		h = x['file'].filename.split('/')[-1] + '::' + datetime.datetime.now().strftime("%Y.%m.%d. %H:%M.%S")
+		try:
+			data[h]=lxml.etree.fromstring(content)
+		except lxml.etree.XMLSyntaxError as ex:
+			t=e.get_template("index.html")
+			return t.render({"error": """Error Parsing XML: %s"""%ex})
+		raise web.seeother('/file/%s'%h)
 
 class process:
-    """ process an  id """
-    def GET(self, id):
-        el = self.get_elements(id)
+	""" process an  id """
+	def GET(self, id):
+		el = self.get_elements(id)
 
-        """ redirect to done if no more elements to work on """
-        if len(el) == 0:
-            t=e.get_template('done.html')
-            return t.render({"id":id})
+		""" redirect to done if no more elements to work on """
+		if len(el) == 0:
+			t=e.get_template('done.html')
+			return t.render({ "id": id, "active": 2, "filename": id.split("::")[0] })
 
-        t=e.get_template('process.html')
-        dd = dict(((i.tag, i.text) for i in el[0].getchildren()))
-        return t.render(dd)
+		t=e.get_template('process.html')
+		dd = dict(((i.tag, i.text) for i in el[0].getchildren()))
+		dd['active'] = 1
+		dd['count'] = len(el)
+		dd['filename'] = id.split("::")[0]
+		return t.render(dd)
 
-    def POST(self,id):
-        x = web.input()
-        el = self.get_elements(id)
+	def POST(self,id):
+		x = web.input()
+		el = self.get_elements(id)
 
-        """ if taxnr is defined well add it """
-        if re.match("[0-9\\-]{8,}", x['taxnr']):
-            ae=lxml.etree.Element('tax_nr')
-            ae.text=x['taxnr']
-            el[0].append(ae)
+		""" if taxnr is defined well add it """
+		if re.match("[0-9\\-]{8,}", x['taxnr']):
+			ae=lxml.etree.Element('tax_nr')
+			ae.text=x['taxnr']
+			el[0].append(ae)
 
-        """ if the orgid is defined add it """
-        if re.match("\\d+", x['orgid']):
-            ae=lxml.etree.Element('org_id')
-            ae.text=x['orgid']
-            el[0].append(ae)
+		""" if the orgid is defined add it """
+		if re.match("\\d+", x['orgid']):
+			ae=lxml.etree.Element('org_id')
+			ae.text=x['orgid']
+			el[0].append(ae)
 
-        """ return the next element """
-        return self.GET(id)
+		""" return the next element """
+		return self.GET(id)
 
-    def get_elements(self,id):
-        """ get the elements to work on """
-        d = data.get(id,None)
-        """ redirect to index if data is not there """
-        if not d:
-            raise web.seeother('/')
+	def get_elements(self,id):
+		""" get the elements to work on """
+		d = data.get(id,None)
+		""" redirect to index if data is not there """
+		if not d:
+			raise web.seeother('/')
 
-        return d.xpath(
-            "//buyer[not(org_id) and not(tax_nr)]") + d.xpath(
-            "//seller[not(org_id) and not(tax_nr)]") + d.xpath(
-            "//tender[not(org_id) and not(tax_nr)]")
+		return d.xpath(
+			"//buyer[not(org_id) and not(tax_nr)]") + d.xpath(
+			"//seller[not(org_id) and not(tax_nr)]") + d.xpath(
+			"//tender[not(org_id) and not(tax_nr)]")
 
 
 class download:
-    def GET(self,id):
-        d = data.get(id,None)
-        """ redirect to index if data is not there """
-        if not d:
-            raise web.seeother('/')
+	def GET(self,id):
+		d = data.get(id,None)
+		""" redirect to index if data is not there """
+		if not d:
+			raise web.seeother('/')
 
-        web.header('Content-Type', 'application/xml')
-        web.header('Content-disposition', 'attachment; filename=%s'%id.split("::")[0])
-        return lxml.etree.tostring(data[id])
+		web.header('Content-Type', 'application/xml')
+		web.header('Content-disposition', 'attachment; filename=%s'%id.split("::")[0])
+		return lxml.etree.tostring(data[id])
 
 
 if __name__=="__main__":
-    app = web.application(urls, globals())
-    app.run()
+	app = web.application(urls, globals())
+	app.run()
